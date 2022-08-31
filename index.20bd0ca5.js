1b24fa4e174852c06c5e786060501fb0285744ee
Roksolana0101 <107188470+Roksolana0101@users.noreply.github.com>
2022-08-31 11:51:17 +0000

    Deploying to gh-pages from @ Roksolana0101/goit-team-project-group07@4837b2290b19de6f7f982cee3984c86c57b56adf 🚀

---
+++ index.20bd0ca5.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.20bd0ca5.js.map
---
+++ index.f301b790.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.f301b790.js.map
